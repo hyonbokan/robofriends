81d5d29b2103f69a42d99c5165b6e286ed63878c
hyonbokan <mkkanhb@gmail.com>
2022-09-27 12:42:43 +0800

    uploading files

---
+++ src/components/Scroll.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: "auto",
+        border: "1px solid black",
+        height: "100vh",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+export default Scroll;
